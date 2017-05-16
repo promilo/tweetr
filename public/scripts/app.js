6d9eafa30d177809c2fc594218dd05146047c3cc
promilo <milind.shah@mail.utoronto.ca>
2017-05-16 18:45:04 +0000

    Fixed some style errors and changed  for of  loop to for each to be more compatible to more browsers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,45 +3,88 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
- var tweetData = {
-   "user": {
-     "name": "Newton",
-     "avatars": {
-       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+ // Fake data taken from tweets.json
+ var data = [
+   {
+     "user": {
+       "name": "Newton",
+       "avatars": {
+         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+       },
+       "handle": "@SirIsaac"
      },
-     "handle": "@SirIsaac"
+     "content": {
+       "text": "If I have seen further it is by standing on the shoulders of giants"
+     },
+     "created_at": 1461116232227
    },
-   "content": {
-     "text": "If I have seen further it is by standing on the shoulders of giants"
+   {
+     "user": {
+       "name": "Descartes",
+       "avatars": {
+         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+       },
+       "handle": "@rd" },
+     "content": {
+       "text": "Je pense , donc je suis"
+     },
+     "created_at": 1461113959088
    },
-   "created_at": 1461116232227
- }
+   {
+     "user": {
+       "name": "Johann von Goethe",
+       "avatars": {
+         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+       },
+       "handle": "@johann49"
+     },
+     "content": {
+       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+     },
+     "created_at": 1461113796368
+   }
+ ];
+
+ function renderTweets(tweets) {
+   // loops through tweets
+     // calls createTweetElement for each tweet
+     // takes return value and appends it to the tweets container
+
+    tweets.forEach( (tweet) =>
+    $('#tweets').append(createTweetElement(tweet)));
+
+  };
 
 
-function createTweetElement(object) {
-  let tweet = $('<article>');
+
+
+
+
+function createTweetElement(aTweet) {
+  let $tweet = $('<article>').addClass('tweet');
   let header =$('<header>')
-  header.append($('<img>').attr("src", object.user.avatars.small))
-  header.append($('<h3>').text(object.user.name));
-  header.append($('<p>').text(object.user.handle));
+  header.append($('<img>').attr("src", aTweet.user.avatars.small))
+  header.append($('<h3>').text(aTweet.user.name));
+  header.append($('<p>').text(aTweet.user.handle));
   let footer = $('<footer>');
-  footer.append($('<p>').text(object.created_at));
+  footer.append($('<p>').text(aTweet.created_at));
   footer.append($('<img>').attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Bot%C3%B3n_Me_gusta.svg/200px-Bot%C3%B3n_Me_gusta.svg.png"));
-  tweet.append(header);
-  tweet.append($('<p>').text(object.context.text));
-  tweet.append(footer);
-  return tweet;
+  $tweet.append(header);
+  $tweet.append($('<p>').text(aTweet.content.text));
+  $tweet.append(footer);
+  return $tweet;
 }
 
 
 
 
  $(document).ready(function() {
-   var $tweet = createTweetElement(tweetData);
-   // Test / driver code (temporary)
-   console.log($tweet); // to see what it looks like
-   $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    renderTweets(data);
 
 });
